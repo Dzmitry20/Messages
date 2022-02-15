cc5a32e3902a03996657be8d5f80e47a588c2501
Dzmitry <zhdv27@mail.ru>
2022-02-15 22:22:22 +0300

    first commit

---
+++ src/main/java/com/example/messages/domain/Message.java
@@ -0,0 +1,51 @@
+package com.example.messages.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String tag;
+
+    public Message() {
+    }
+
+
+
+    public Message(String name, String tag) {
+        this.name = name;
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
---
+++ src/main/java/com/example/messages/domain/Role.java
@@ -0,0 +1,6 @@
+package com.example.messages.domain;
+
+public enum Role {
+
+    USER;
+}
